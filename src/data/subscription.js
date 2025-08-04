9065fcc06c03b4e542b566da63c943a802fa6d5e
Dkwtf12 <121540365+Dkwtf12@users.noreply.github.com>
2025-08-04 12:39:12 +0530

    Create subscription.js

---
+++ src/data/subscription.js
@@ -0,0 +1,32 @@
+const subscriptions = [
+  {
+    name: "Netflix Premium",
+    duration: "1 Month",
+    originalPrice: "₹499",
+    offerPrice: "₹199",
+    note: "Shared, 4K Ultra HD",
+  },
+  {
+    name: "Amazon Prime",
+    duration: "6 Months",
+    originalPrice: "₹899",
+    offerPrice: "₹349",
+    note: "Delivered on your Email",
+  },
+  {
+    name: "Telegram Premium",
+    duration: "1 Month",
+    originalPrice: "₹469",
+    offerPrice: "₹199",
+    note: "Direct Gift Link",
+  },
+  {
+    name: "NordVPN",
+    duration: "1 Year",
+    originalPrice: "₹3499",
+    offerPrice: "₹999",
+    note: "Private Login",
+  }
+];
+
+export default subscriptions;
